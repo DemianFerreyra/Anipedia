0e6c568c8e178a360028997484102dd051c2ad7e
DemianFerreyra <demiancra@gmail.com>
2022-03-19 18:41:54 -0300

    actualizacion

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 var datos = [{titulo: 'tokyo revengers', desc: 'god 10/10'}];
 datos.unshift({titulo: 'naruto', desc: 'naruto ananashe'})
 datos.unshift({titulo: 'dragon ball', desc: 'dragon ball ananashe deaaaaaaaa dead dae a das da sad a ads '})
-console.log(datos)
 
 
     var dynamic = document.querySelector('.resultado');  
@@ -12,4 +11,13 @@ console.log(datos)
       <h1>${datos[i].titulo}</h1>
       <p>${datos[i].desc}</p>
       </div>` + fetch ;
+}
+var dynamic = document.querySelector('.recomendaciones');  
+for (var i = 0; i < datos.length; i++) {
+  var fetch = document.querySelector('.recomendaciones').innerHTML;  
+  dynamic.innerHTML = `<div class="tarjeta">
+  <img src=${"/imagenes/sample.jpg"} class="imagen" alt="">
+  <h1>${datos[i].titulo}</h1>
+  <p>${datos[i].desc}</p>
+  </div>` + fetch ;
 }
